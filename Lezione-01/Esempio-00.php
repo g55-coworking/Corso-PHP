683b1e6cab0de88fdb2cad56b5688ff3b9d6661a
francescobianco <info.francescobianco@gmail.com>
2017-02-07 15:33:50 +0100

    niziamo la lezione 3

---
+++ Lezione-01/Esempio-00.php
@@ -0,0 +1,10 @@
+// commento di linea
+
+/*
+commento di blocco
+commento di blocco
+commento di blocco
+commento di blocco
+*/
+
+# commento di line
---
+++ Lezione-01/Esempio-01.php
@@ -0,0 +1,29 @@
+<?php
+
+// catturo 'a'
+$a = $_GET['a'];
+
+// assegna $a su $c
+$c = $a[0];
+
+// stampo $c
+echo $c;
+
+?>
+
+<form action="prova.php">
+
+a:<input type="text" name="a">
+
+<input type="submit">
+
+</form>
+
+
+
+
+
+
+
+
+
